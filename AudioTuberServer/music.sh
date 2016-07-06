d16aeadd710224ab3477733020b9913155e270ed
tmanthegamer <tmanthegamer@gmail.com>
2016-07-05 20:49:49 -0700

    Initial setup for the Client and Server.

---
+++ AudioTuberServer/music.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+URL=${1}
+TUBER="/home/tyler/AudioTuber/AudioTuberServer"
+STORAGE="storage/%(title)s.%(ext)s"
+
+if [ $# -ne 1 ]; then
+    #chmod +x ${RIX_FILE}.sh
+    echo $0: usage: music.sh pants
+    exit 1
+fi
+
+#youtube-dl -q -f 140 -o ${TUBER}/${STORAGE} ${URL}
+
+youtube-dl -f 'bestaudio[ext=m4a]' -o ${TUBER}/${STORAGE} ${URL} 
+
+sleep 10
+echo "Goodbye"
+
---
+++ AudioTuberServer/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+TUBEPATH=$(pwd)
+export  TUBER=${TUBEPATH}
+sudo ln -s "$TUBER/music" /usr/bin/
