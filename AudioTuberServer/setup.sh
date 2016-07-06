0a3dc44fe6f85b21838632be786e905bb969aaae
tmanthegamer <tmanthegamer@gmail.com>
2016-07-05 20:53:20 -0700

    Commented out the setup.sh and music until music.sh is fully completed.

--- AudioTuberServer/setup.sh
+++ AudioTuberServer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-TUBEPATH=$(pwd)
-export  TUBER=${TUBEPATH}
-sudo ln -s "$TUBER/music" /usr/bin/
+#Initial set up file that 
+#TUBEPATH=$(pwd)
+#export  TUBER=${TUBEPATH}
+#sudo ln -s "$TUBER/music" /usr/bin/
